be89760fda71bf16c13c07eb603610ac496c2384
Joachim Wagner <joachimwagner93@gmail.com>
2022-06-18 17:39:17 +0200

    update skill analysis

--- webapp.py
+++ webapp.py
@@ -59,33 +59,71 @@ def region_analysis(df_filtered, geo_data):
             st.plotly_chart(fig)
 
 def requirements_analysis(df_requirements):
-    st.title("Distribution of Requirements for Data Science Jobs in Germany")
-    col1, col2 = st.columns([1, 2])
-    choices = ["Data Scientist", "Data Analyst", "Data Engineer", "Machine Learning Engineer", "Software Engineer",
+    st.title("Requirements for Data Science Jobs in Germany")
+    col1, col2, col3 = st.columns(3)
+    choices = ["All", "Data Scientist", "Data Analyst", "Data Engineer", "Machine Learning Engineer", "Software Engineer",
                "Data Science Consultant", "Manager"]
 
     with col1:
-        st.title("")
-        st.markdown("#### Please select the jobs you're interested in.")
-        st.title("")
+        selected_skills = st.selectbox("Which Skills are you interested in?",
+                                       ("All", "Languages", "Technologies", "Libraries"))
 
-        # check_ds = st.checkbox("Data Scientist", value=True)
-        # check_da = st.checkbox("Data Analyst", value=True)
-        # check_de = st.checkbox("Data Engineer", value=True)
-        # check_mle = st.checkbox("Machine Learning Engineer", value=True)
-        # check_se = st.checkbox("Software Engineer", value=True)
-        # check_dsc = st.checkbox("Data Science Consultant", value=True)
-        # check_m = st.checkbox("Manager", value=True)
-
-        choice = st.radio("Pages", options=choices)
-
-    df_requirements = df_requirements.loc[df_requirements["title_cat"] == choice]
-    percentages = (df_requirements.drop("title_cat", axis=1).sum().sort_values(ascending=False) * 100 / len(df_requirements)).apply(lambda x: round(x,2))
-    fig = px.bar(percentages[:20], title="Most required Skills for Data Science Jobs",
-                 labels={"value": "Percentages", "index": "Skills"})
-    fig.layout.update(showlegend=False)
     with col2:
-        st.plotly_chart(fig)
+        first_choice = st.selectbox("Which Jobtitle are you interested in?", choices)
+
+    choices_new = [choice for choice in choices if choice != first_choice]
+
+    with col3:
+        second_choice = st.selectbox("Choose a second Jobtitle (optional)", ["None"] + choices_new)
+
+    if second_choice == "None":
+        choices_selected = [first_choice]
+    else:
+        choices_selected = [first_choice, second_choice]
+
+    if first_choice != "All":
+        df_first = df_requirements.loc[df_requirements["title_cat"] == first_choice]
+    else:
+        df_first = df_requirements
+    percentages_first = pd.Series(
+        df_first.drop("title_cat", axis=1).sum() * 100 / len(df_first),
+        name=first_choice).to_frame()
+    percentages_first["type"] = 19 * ["Languages"] + 20 * ["Technologies"] + 14 * ["Libraries"]
+
+    if second_choice == "None":
+        percentages = percentages_first
+    else:
+        if second_choice != "All":
+            df_second = df_requirements.loc[df_requirements["title_cat"] == second_choice]
+        else:
+            df_second = df_requirements
+        percentages_second = pd.Series(
+            df_second.drop("title_cat", axis=1).sum() * 100 / len(df_second),
+            name=second_choice).to_frame()
+        percentages = pd.merge(percentages_first, percentages_second, left_index=True, right_index=True)
+
+    percentages = percentages.reset_index()
+    percentages[first_choice] = percentages[first_choice].apply(lambda x: round(x, 2))
+    if second_choice != "None":
+        percentages = percentages.loc[(percentages[first_choice] > 0) | (percentages[second_choice] > 0)]
+        percentages[second_choice] = percentages[second_choice].apply(lambda x: round(x, 2))
+    else:
+        percentages = percentages.loc[percentages[first_choice] > 0]
+    percentages = percentages.sort_values(choices_selected, ascending=False)
+
+    if selected_skills != "All":
+        percentages = percentages.loc[percentages["type"] == selected_skills]
+
+    fig = px.bar(percentages,
+                 x="index",
+                 y=choices_selected,
+                 # title="Most required Skills for Data Science Jobs",
+                 labels={"value": "Percentages"},
+                 barmode="group",
+                 width=1050, height=500)
+    fig.update_layout(legend={"yanchor":"top", "y": 0.99, "xanchor": "right", "x": 0.99})
+    st.plotly_chart(fig)
+
 
 df_filtered, geo_data, df_requirements = load_data()
 
@@ -93,7 +131,7 @@ st.sidebar.write("Sidebar")
 options = st.sidebar.radio("Pages", options=["Home", "Requirements Analysis", "Region Analysis"])
 if options == "Requirements Analysis":
     requirements_analysis(df_requirements)
-if options == "Region Analysis":
+elif options == "Region Analysis":
     region_analysis(df_filtered, geo_data)
 else:
     st.write("TODO")
