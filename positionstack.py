36f8a04fa41310fd83fa43cacfffe7ac239977dc
Joachim Wagner <joachimwagner93@gmail.com>
2022-09-11 22:44:41 +0200

    added preprocessing script

--- positionstack.py
+++ positionstack.py
@@ -6,23 +6,29 @@ import concurrent.futures
 import config
 
 
-def get_location(location):
+def main(df):
+    print("get geo data")
     api_key = config.positionstack_key
-    url = f"http://api.positionstack.com/v1/forward?access_key={api_key}&query={location}&limit=1&country=DE"
+    locations = df["location"].unique()
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        geo_data = list(tqdm(executor.map(lambda x: get_location(x, api_key), locations), total=len(locations)))
+    geo_data = pd.concat(geo_data)
+    geo_data.to_csv("data/geo_data.csv", index=False)
+    return None
+
+
+def get_location(location, key):
+    url = f"http://api.positionstack.com/v1/forward?access_key={key}&query={location}&limit=1&country=DE"
     try:
         results = pd.DataFrame(requests.get(url).json()["data"])
-        results["location"] = [location]
-    except:
+    except KeyError:
         pass
-    return results
+    else:
+        results["location"] = [location]
+        return results
+    return None
 
 
-df = pd.read_csv("data2/cleaned_long2.csv")
-locations = df["location"].unique()
-
-with concurrent.futures.ThreadPoolExecutor() as executor:
-    geo_data = list(tqdm(executor.map(get_location, locations), total=len(locations)))
-
-geo_data = pd.concat(geo_data)
-
-geo_data.to_csv("data2/geo_data2.csv", index=False)
+if __name__ == "__main__":
+    data = pd.read_csv("data2/cleaned_long2.csv")
+    main(data)
